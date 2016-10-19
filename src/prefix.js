72a7dfd993f288359f514fee81df5f733e565ffb
Dave Landry <landry.dave@gmail.com>
2016-10-18 23:15:41 -0400

    adds jsdoc for prefix function

--- src/prefix.js
+++ src/prefix.js
@@ -1,11 +1,11 @@
-let val = undefined;
-
+/**
+    @function prefix
+    @desc Returns the appropriate vendor prefix to use in CSS styles.
+*/
 export default function() {
-  if (val !== void 0) return val;
-  if ("-webkit-transform" in document.body.style) val = "-webkit-";
-  else if ("-moz-transform" in document.body.style) val = "-moz-";
-  else if ("-ms-transform" in document.body.style) val = "-ms-";
-  else if ("-o-transform" in document.body.style) val = "-o-";
-  else val = "";
-  return val;
+  if ("-webkit-transform" in document.body.style) return "-webkit-";
+  else if ("-moz-transform" in document.body.style) return "-moz-";
+  else if ("-ms-transform" in document.body.style) return "-ms-";
+  else if ("-o-transform" in document.body.style) return "-o-";
+  else return "";
 }
